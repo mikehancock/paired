f7f11c5b1700586c83123390ecc62cf0c6443cf4
Michael Hancock <michael.hancock@scribestar.com>
2014-07-06 18:44:13 +0100

    added stylecop

--- src/Mgnd.Paired.Web/App_Start/IdentityConfig.cs
+++ src/Mgnd.Paired.Web/App_Start/IdentityConfig.cs
@@ -8,7 +8,6 @@ using Mgnd.Paired.Web.Models;
 namespace Mgnd.Paired.Web
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
